204c73c294da886425da3baa189337c9bc551c82
kadegel <kdegelsmith9764@student.palomar.edu>
2025-05-14 10:56:15 -0700

    add read_data.R file to make variables

---
+++ read_data.R
@@ -0,0 +1,7 @@
+# Read csv data
+confirmed_global <- read_csv('confirmed_global.csv')
+deaths_global <- read_csv('deaths_global.csv')
+recovered_global <- read_csv('recovered_global.csv')
+
+# confirmed_us <- read_csv()
+# deaths_us <- read_csv()
