7200cd2bcb002c086bc1b4837b8eb1c85bd48357
kadegel <kdegelsmith9764@student.palomar.edu>
2025-05-14 10:55:24 -0700

    add shell file to download global data

---
+++ global_data.sh
@@ -0,0 +1,6 @@
+# Download global data, added by Kira Degelsmith
+curl -L -o confirmed_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/refs/heads/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+
+curl -L -o deaths_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/refs/heads/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+
+curl -L -o recovered_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/refs/heads/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv
